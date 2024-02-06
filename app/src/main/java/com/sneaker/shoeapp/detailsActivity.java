721687b9da4a9549cca48ddcfb4965901edf1a9e
NguynThuHigh <minhnguyen11a1cmg@gmail.com>
2024-02-06 16:35:05 +0700

    update checkout

---
+++ app/src/main/java/com/sneaker/shoeapp/detailsActivity.java
@@ -0,0 +1,14 @@
+package com.sneaker.shoeapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class detailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details);
+    }
+}
