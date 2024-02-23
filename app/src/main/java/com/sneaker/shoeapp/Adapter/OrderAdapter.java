16e1509401e95b55406ca5fb6c47082014e74be9
NguynThuHigh <minhnguyen11a1cmg@gmail.com>
2024-02-23 11:57:53 +0700

    order details

--- app/src/main/java/com/sneaker/shoeapp/Adapter/OrderAdapter.java
+++ app/src/main/java/com/sneaker/shoeapp/Adapter/OrderAdapter.java
@@ -5,11 +5,13 @@ package com.sneaker.shoeapp.Adapter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.sneaker.shoeapp.Interface.ClickItemOrder;
 import com.sneaker.shoeapp.R;
 import com.sneaker.shoeapp.model.Order;
 
@@ -17,12 +19,14 @@ import java.util.List;
 
 public class OrderAdapter extends  RecyclerView.Adapter<OrderAdapter.OrderViewHolder>{
     List<Order> orderList;
+    private ClickItemOrder clickItemOrder;
 
     public OrderAdapter(List<Order> orderList) {
         this.orderList = orderList;
     }
 
-    public void setData(List<Order> orderList){
+    public void setData(List<Order> orderList,ClickItemOrder clickItemOrder){
+        this.clickItemOrder = clickItemOrder;
         this.orderList = orderList;
         notifyDataSetChanged();
     }
@@ -43,6 +47,12 @@ public class OrderAdapter extends  RecyclerView.Adapter<OrderAdapter.OrderViewHo
             holder.Order_Status.setText("Delivered");
         }
         holder.Order_Value.setText("$"+order.getTotal_value());
+        holder.item_order.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                clickItemOrder.onClickedItem(order);
+            }
+        });
 
     }
 
@@ -58,6 +68,7 @@ public class OrderAdapter extends  RecyclerView.Adapter<OrderAdapter.OrderViewHo
 
     public class OrderViewHolder extends RecyclerView.ViewHolder{
         TextView Order_ID,Order_Date, Order_Status, Order_Quantity, Order_Value;
+        FrameLayout item_order;
         public OrderViewHolder(@NonNull View itemView) {
             super(itemView);
             Order_ID = itemView.findViewById(R.id.Order_ID);
@@ -65,6 +76,7 @@ public class OrderAdapter extends  RecyclerView.Adapter<OrderAdapter.OrderViewHo
             Order_Status = itemView.findViewById(R.id.Order_Status);
             Order_Quantity = itemView.findViewById(R.id.Order_Quantity);
             Order_Value = itemView.findViewById(R.id.Order_Value);
+            item_order = itemView.findViewById(R.id.item_order);
         }
     }
 }
---
+++ app/src/main/java/com/sneaker/shoeapp/Interface/ClickItemOrder.java
@@ -0,0 +1,7 @@
+package com.sneaker.shoeapp.Interface;
+
+import com.sneaker.shoeapp.model.Order;
+
+public interface ClickItemOrder {
+    void onClickedItem(Order order);
+}
