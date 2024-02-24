b3dd320d7bd3d02de3a43cee1640699f9c4647fd
unknown <you@example.com>
2024-02-24 21:13:33 +0700

    update content

--- app/src/main/java/com/sneaker/shoeapp/EditProfileActivity.java
+++ app/src/main/java/com/sneaker/shoeapp/EditProfileActivity.java
@@ -3,6 +3,7 @@ package com.sneaker.shoeapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -11,38 +12,66 @@ import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
-import com.sneaker.shoeapp.Dialog.DialogRename;
 
 public class EditProfileActivity extends AppCompatActivity {
 
-    Button btnConfirm,btnRename;
+    Button btnRename,btnConfirm,btnChangeAvt,btnChangeEmail,btnChangePassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
         addControls();
-        addEvent();
-    }
+        btnConfirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
 
-    private void addEvent() {
         btnRename.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(EditProfileActivity.this, "ok ok", Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(EditProfileActivity.this, DialogEditUsernameActivity.class);
+                startActivity(intent);
+            }
+        });
+        btnChangeAvt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(EditProfileActivity.this, DialogChangeAvatarActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnChangeEmail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(EditProfileActivity.this, DialogChangeEmailActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnChangePassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(EditProfileActivity.this, DialogChangePasswordActivity.class);
+                startActivity(intent);
             }
         });
     }
 
 
+
+
     private void addControls() {
         btnRename= findViewById(R.id.btnRename);
         btnConfirm=findViewById(R.id.btnConfirm);
+        btnChangeAvt=findViewById(R.id.btnChangeAvt);
+        btnChangeEmail=findViewById(R.id.btnChangeEmail);
+        btnChangePassword=findViewById(R.id.btnChangePassword);
 
     }
 
-    private void reName() {
-        DialogRename dialogRename = new DialogRename();
-        dialogRename.show(getSupportFragmentManager(), "dialog rename");
-    }
+
 }
