8a01e50ab7a5127e0d0a75142742631f0a708058
phucpham13123 <phucpham21109@gmail.com>
2024-03-20 21:30:33 +0700

    new fixed

--- app/src/main/java/com/sneaker/shoeapp/FormConfirmEmailActivity.java
+++ app/src/main/java/com/sneaker/shoeapp/FormConfirmEmailActivity.java
@@ -2,13 +2,15 @@ package com.sneaker.shoeapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 
 public class FormConfirmEmailActivity extends AppCompatActivity {
-    ImageButton btnBacktoLogin;
+    Button btnBacktoLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +21,8 @@ public class FormConfirmEmailActivity extends AppCompatActivity {
         btnBacktoLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                finish();
+                Intent intent = new Intent( FormConfirmEmailActivity.this, LoginActivity.class);
+                startActivity(intent);
             }
         });
     }
